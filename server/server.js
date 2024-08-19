0f4c995c1135436c217658a6478ed24b6e49f9c6
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-18 22:23:05 -0500

    updated server to correct imported module names

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo-server');
-const { expressMiddleware } = require('@apollo/server/express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
