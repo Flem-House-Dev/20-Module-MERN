e59eab061b9ab8ffc92176a76d09b9b92951cd85
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-18 22:32:47 -0500

    updated server.js to start apollo server properly

--- server/server.js
+++ server/server.js
@@ -15,23 +15,30 @@ const server = new ApolloServer({
   context: expressMiddleware,
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+  await server.start();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+
+  app.use('graphql', expressMiddleware(server));
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
-}
+};
 
-app.use('graphql', expressMiddleware(server));
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-  });
-});
+startApolloServer();
